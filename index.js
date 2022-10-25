cc0bb584e9123ec3e8db02c07c069e22866d1ac5
rabiul155 <siyamict@gmail.com>
2022-10-25 20:18:49 +0600

    cource file added

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 
+
+const cources = require('./data/cources.json')
+
 app.get('/', (req, res) => {
     res.send('hello buddy i am your server ');
-
 })
+
+app.get('/cources', (req, res) => {
+    res.send(cources);
+})
+
+
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
